25c5605dbf427999e5e7c8b93966b7bce7e9815b
RG <rgeorgiev001@gmail.com>
2023-03-20 11:27:00 +0000

    Set up Node.js backend and sign up users

--- WebstormProjects/unimeetup/client/src/components/Auth.js
+++ WebstormProjects/unimeetup/client/src/components/Auth.js
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+
 
 const Auth = ({ setShowModal,  isSignUp }) => {
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
     const [confirmPassword, setConfirmPassword] = useState(null)
     const [error, setError] = useState(null)
+    const [ cookies, setCookie, removeCookie] = useCookies(null)
+
+    let navigate = useNavigate()
 
     console.log(email, password, confirmPassword)
 
@@ -19,9 +26,60 @@ const Auth = ({ setShowModal,  isSignUp }) => {
                 setError('Passwords do not match!')
                 return
             }
+            const response = await axios.post(`http://localhost:8000/${isSignUp ? 'signup' : 'login'}`, { email, password })
+
+            setCookie('AuthToken', response.data.token)
+            setCookie('UserId', response.data.userId)
+
+            const success = response.status === 201
+            if (success && isSignUp) navigate ('/signup')
+            if (success && !isSignUp) navigate ('/dashboard')
+
+            window.location.reload()
 
         } catch (error) {
             console.log(error)
         }
-    }}
+    }
+
+    return (
+        <div className="auth">
+            <div className="close-icon" onClick={handleClick}>ⓧ</div>
+
+            <h2>{isSignUp ? 'REGISTER': 'LOG IN'}</h2>
+            <p>By registering, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="email"
+                    pattern="^[^ ]+@city.ac.uk"
+                    required={true}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="password"
+                    required={true}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {isSignUp && <input
+                    type="password"
+                    id="password-check"
+                    name="password-check"
+                    placeholder="confirm password"
+                    required={true}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />}
+                <input className="secondary-button" type="submit"/>
+                <p>{error}</p>
+            </form>
+            <hr/>
+            <h2>Meet students nearby ®</h2>
+        </div>
+    )
+}
 export default Auth
---
+++ WebstormProjects/unimeetup/server/index.js
@@ -0,0 +1,90 @@
+const PORT = 8000
+const express = require('express')
+const {MongoClient} = require('mongodb')
+const {v4: uuidv4} = require('uuid')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+require('dotenv').config()
+
+const uri = 'mongodb+srv://rtgeorgiev:mypassword@Cluster0.hkj2v1s.mongodb.net/?retryWrites=true&w=majority'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Default
+app.get('/', (req, res) => {
+    res.json('Hello to my app')
+})
+
+// Sign up to the Database
+app.post('/signup', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {email, password} = req.body
+
+    const generatedUserId = uuidv4()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const existingUser = await users.findOne({email})
+
+        if (existingUser) {
+            return res.status(409).send('User already exists. Please login')
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generatedUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword
+        }
+
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 60 * 24
+        })
+        res.status(201).json({token, userId: generatedUserId})
+
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close()
+    }
+})
+
+// Log in to the Database
+app.post('/login', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {email, password} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const user = await users.findOne({email})
+
+        const correctPassword = await bcrypt.compare(password, user.hashed_password)
+
+        if (user && correctPassword) {
+            const token = jwt.sign(user, email, {
+                expiresIn: 60 * 24
+            })
+            res.status(201).json({token, userId: user.user_id})
+        }
+
+        res.status(400).json('Invalid Credentials')
+
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.close()
+    }
+})
